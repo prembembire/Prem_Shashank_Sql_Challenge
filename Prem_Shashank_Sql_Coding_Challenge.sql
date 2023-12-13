c858858168b58fc047f2d941beb62e2497415179
prembembire <122213524+prembembire@users.noreply.github.com>
2023-12-13 12:47:34 +0530

    Update Prem_Shashank_Sql_Coding_Challenge.sql

--- Prem_Shashank_Sql_Coding_Challenge.sql
+++ Prem_Shashank_Sql_Coding_Challenge.sql
@@ -1,4 +1,4 @@
-/*ECOMMERCE – SQL Coding Challenge 6*/
+/*ECOMMERCE â€“ SQL Coding Challenge 6*/
 Create Database Ecommerce
 Use Ecommerce
 
@@ -54,7 +54,19 @@ Quantity Int,
 Foreign Key (Order_Id) References Orders(Order_Id),
 Foreign Key (Product_Id) References Products(Product_Id)
 );
-Select * from Customers;Select * from Products;Select * from Cart;Select * from Orders;Select * from Order_Items;--Inserting Values to Tables--Customers TableInsert Into Customers(Customer_Id,Name,Email,Password)Values(1 ,'John Doe' ,'johndoe@example.com' ,'123 Main St, City'),
+
+Select * from Customers;
+Select * from Products;
+Select * from Cart;
+Select * from Orders;
+Select * from Order_Items;
+
+
+--Inserting Values to Tables
+--Customers Table
+Insert Into Customers(Customer_Id,Name,Email,Password)
+Values
+(1 ,'John Doe' ,'johndoe@example.com' ,'123 Main St, City'),
 (2 ,'Jane Smith', 'janesmith@example.com', '456 Elm St, Town'),
 (3 ,'Robert Johnson' ,'robert@example.com ','789 Oak St, Village'),
 (4 ,'Sarah Brown', 'sarah@example.com', '101 Pine St, Suburb'),
@@ -63,7 +75,14 @@ Foreign Key (Product_Id) References Products(Product_Id)
 (7 ,'Michael Davis', 'michael@example.com', '890 Maple St, State'),
 (8 ,'Emma Wilson', 'emma@example.com', '321 Redwood St, Country'),
 (9 ,'William Taylor', 'william@example.com','432 Spruce St, Province'),
-(10 ,'Olivia Adams', 'olivia@example.com', '765 Fir St, Territory');Select * from Customers--Products TableInsert Into Products(Product_Id,Name,Description,Price,StockQuantity)Values   (1, 'Laptop', 'High-performance laptop', 800.00, 10),
+(10 ,'Olivia Adams', 'olivia@example.com', '765 Fir St, Territory');
+
+Select * from Customers
+
+--Products Table
+Insert Into Products(Product_Id,Name,Description,Price,StockQuantity)
+Values
+   (1, 'Laptop', 'High-performance laptop', 800.00, 10),
    (2, 'Smartphone', 'Latest smartphone', 600.00, 15),
    (3, 'Tablet', 'Portable tablet', 300.00, 20),
    (4, 'Headphones', 'Noise-canceling', 150.00, 30),
@@ -72,7 +91,14 @@ Foreign Key (Product_Id) References Products(Product_Id)
    (7, 'Refrigerator', 'Energy-efficient', 700.00, 10),
    (8, 'Microwave Oven', 'Countertop microwave', 80.00, 15),
    (9, 'Blender', 'High-speed blender', 70.00, 20),
-   (10, 'Vacuum Cleaner', 'Bagless vacuum cleaner', 120.00, 10);Select * from Products--Carts TableInsert Into Cart(Cart_Id,Customer_Id,Product_Id,Quantity)Values(1,1, 1, 2),
+   (10, 'Vacuum Cleaner', 'Bagless vacuum cleaner', 120.00, 10);
+
+Select * from Products
+
+--Carts Table
+Insert Into Cart(Cart_Id,Customer_Id,Product_Id,Quantity)
+Values
+(1,1, 1, 2),
 (2, 1 ,3, 1),
 (3, 2, 2, 3),
 (4 ,3 ,4 ,4),
@@ -81,7 +107,14 @@ Foreign Key (Product_Id) References Products(Product_Id)
 (7 ,5 ,1, 1),
 (8 ,6 ,10, 2),
 (9 ,6, 9 ,3),
-(10 ,7 ,7 ,2);Select * from Cart;--Orders TableINSERT INTO orders (Order_id, Customer_id, Order_date, Total_price)
+(10 ,7 ,7 ,2);
+
+
+Select * from Cart;
+
+
+--Orders Table
+INSERT INTO orders (Order_id, Customer_id, Order_date, Total_price)
 VALUES
    (1, 1, '2023-01-05', 1200.00),
    (2, 2, '2023-02-10', 900.00),
@@ -92,7 +125,13 @@ VALUES
    (7, 7, '2023-07-05', 700.00),
    (8, 8, '2023-08-10', 160.00),
    (9, 9, '2023-09-15', 140.00),
-   (10, 10, '2023-10-20', 1400.00);Select * from Orders;--Order_Items TableAlter table order_items
+   (10, 10, '2023-10-20', 1400.00);
+
+Select * from Orders;
+
+--Order_Items Table
+
+Alter table order_items
 Add item_amount int;
 
 INSERT INTO order_items (order_item_id, order_id, product_id, quantity, item_amount)
@@ -106,7 +145,21 @@ VALUES
    (7, 5, 1, 1, 800.00),
    (8, 5, 2, 2, 1200.00),
    (9, 6, 10, 2, 240.00),
-   (10, 6, 9, 3, 210.00);Select * from Order_Items;Select * from Customers;Select * from Products;Select * from Cart;Select * from Orders;Select * from Order_Items;--1. Update refrigerator product price to 800.
+   (10, 6, 9, 3, 210.00);
+
+Select * from Order_Items;
+
+
+
+
+
+Select * from Customers;
+Select * from Products;
+Select * from Cart;
+Select * from Orders;
+Select * from Order_Items;
+
+--1. Update refrigerator product price to 800.
 Update Products Set Price=800
 Where(Name='Refrigerator')
 
@@ -132,7 +185,7 @@ Where Total_Price Between 500 And 1000;
 
 
 
---6. Find Products which name end with letter ‘r’.
+--6. Find Products which name end with letter â€˜râ€™.
 Select * from Products
 Where Name Like '%r';
 
@@ -209,4 +262,7 @@ Where StockQuantity in (Select StockQuantity from Products Where (StockQuantity<
 
 
 
---19. Subquery to Find Customers Who Placed High-Value Ordersselect Name As Customers_With_High_Value_Orders from CustomersWhere Customer_Id In (Select Customer_Id from Orders Where (Total_Price>=1000 ));
+--19. Subquery to Find Customers Who Placed High-Value Orders
+
+select Name As Customers_With_High_Value_Orders from Customers
+Where Customer_Id In (Select Customer_Id from Orders Where (Total_Price>=1000 ));
